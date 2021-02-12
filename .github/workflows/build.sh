ba25403f81a8a86e7bbc029d1494b409fbdfbb53
Andrey Satarin <asatarin@users.noreply.github.com>
2021-02-12 08:41:26 -0800

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-echo Awesome Bot
+set -e
 
 echo awesome_bot --allow-dupe --allow-ssl --allow-redirect $(find /github/workspace/ -name "*.md")
 
 awesome_bot --allow-dupe --allow-ssl --allow-redirect $(find /github/workspace/ -name "*.md")
 
-echo Awesome Bot
