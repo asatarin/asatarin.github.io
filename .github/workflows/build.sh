ef5d348290b322f8e4a53f34e5eeeea367b31fae
Andrey Satarin <asatarin@users.noreply.github.com>
2023-09-08 17:39:02 -0700

    Allow list dl.acm.org website
    dl.acm.org started returning 403 for bot HTTP requests

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-awesome_bot --white-list "https://www.amazon.com" --allow 999 --allow-dupe --allow-redirect $(find /github/workspace/ -name "*.md")
+awesome_bot --white-list "www.amazon.com,dl.acm.org" --allow 999 --allow-dupe --allow-redirect $(find /github/workspace/ -name "*.md")
