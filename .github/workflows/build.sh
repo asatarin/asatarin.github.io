06a4fcaa272e77ebc8cfe4786e919c3131cddfee
Andrey Satarin <asatarin@yandex.ru>
2023-12-22 11:08:17 -0800

    Allow list `medium.com` for link checker

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 awesome_bot \
-  --white-list "www.amazon.com,dl.acm.org,docs.google.com" \
+  --white-list "www.amazon.com,dl.acm.org,docs.google.com,medium.com" \
   --allow 999 \
   --allow-dupe \
   --allow-redirect $(find /github/workspace/ -name "*.md")
