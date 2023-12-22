dfe6879391fba1d63818c52f0c340c1cbd0c4252
Andrey Satarin <asatarin@yandex.ru>
2023-12-22 11:09:17 -0800

    Fix shellcheck complaint

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -6,4 +6,5 @@ awesome_bot \
   --white-list "www.amazon.com,dl.acm.org,docs.google.com,medium.com" \
   --allow 999 \
   --allow-dupe \
-  --allow-redirect $(find /github/workspace/ -name "*.md")
+  --allow-redirect \
+  "$(find /github/workspace/ -name "*.md")"
