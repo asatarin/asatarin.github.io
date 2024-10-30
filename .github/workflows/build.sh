17b605f8382421a45e48c85875bde2d0907fa6ee
Andrey Satarin <asatarin@users.noreply.github.com>
2024-10-29 22:41:41 -0700

    Allow error 429 Too Many Requests

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,7 +5,7 @@ set -e
 # shellcheck disable=SC2046
 awesome_bot \
   --white-list "www.amazon.com,dl.acm.org,docs.google.com,medium.com,substack.com" \
-  --allow 999 \
+  --allow 429,999 \
   --allow-dupe \
   --allow-redirect \
   $(find /github/workspace/ -name "*.md")
